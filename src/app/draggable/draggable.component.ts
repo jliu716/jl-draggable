05730c81e73043b84aaef4bdcdf4c57d18d4ea42
Jiayi Liu <liujiayi19920724@gmail.com>
2022-12-13 21:10:12 +1300

    add placeholder draggable component

---
+++ src/app/draggable/draggable.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "jl-draggable",
+  templateUrl: "./draggable.component.html",
+  styleUrls: ["./draggable.component.less"],
+})
+export class DraggableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
