1c4af5cb6228d26a2957b3532cd52a73e5a60e8f
Jiayi Liu <liujiayi19920724@gmail.com>
2022-12-13 21:22:29 +1300

    add a box to draggable

--- src/app/draggable/draggable.component.ts
+++ src/app/draggable/draggable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "jl-draggable",
@@ -6,6 +6,16 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./draggable.component.less"],
 })
 export class DraggableComponent implements OnInit {
+  @Input("width") width: number = 50;
+  @Input("height") height: number = 50;
+  @Input("left") left: number = 0;
+  @Input("top") top: number = 0;
+  @Input("color") color: string = "";
+
+  get transform() {
+    return "translate3d(" + this.left + "px," + this.top + "px," + "0px)";
+  }
+
   constructor() {}
 
   ngOnInit() {}
